cf7f5123b1e21971e67e30cbc430380998e86a8a
jcpinheiro <jcpinheiro@ifma.edu.br>
2020-01-21 10:23:57 -0300

    Primeiro Commit

---
+++ src/main/java/com/example/testees2/Testees2Application.java
@@ -0,0 +1,13 @@
+package com.example.testees2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testees2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testees2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testees2/Testees2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testees2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testees2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
